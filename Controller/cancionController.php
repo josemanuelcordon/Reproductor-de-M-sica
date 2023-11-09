97d3d8dec2e84bb2a6946d3650d0eb51c2e26b0c
josemanuelcordon <josemanuelcordoncastillo01@gmail.com>
2023-11-09 13:06:34 +0100

    add feature admin can edit songs

--- Controller/cancionController.php
+++ Controller/cancionController.php
@@ -7,13 +7,21 @@ if (!empty($_POST['song-title'])) {
     $id_creator = $_SESSION['user']->getId();
     if ($_FILES['song-mp3']) {
         $mp3 = "assets/music/" . $title . ".mp3";
-        $destino = "assets/music/" . $mp3;
-        move_uploaded_file($_FILES["song-mp3"]["tmp_name"], $destino);
+        move_uploaded_file($_FILES["song-mp3"]["tmp_name"], $mp3);
     } else {
         $mp3 = "Todavia no disponible";
     }
     CancionRepository::createSong($playlist_id, $title, $duration, $author, $id_creator, $mp3);
     header('location: index.php?c=cancion&pl-id=' . $_GET['pl-id']);
+}
+
+if (!empty($_POST['song-title-edit'])) {
+    $id = intval($_POST['song-id']);
+    $title = $_POST['song-title-edit'];
+    $author = $_POST['song-author-edit'];
+    $mp3 = "assets/music/" . $title . ".mp3";
+    move_uploaded_file($_FILES["song-mp3-edit"]["tmp_name"], $mp3);
+    CancionRepository::updateSong($id, $title, $author, $mp3);
 }
 
 if (!empty($_GET['song-search'])) {
@@ -28,6 +36,27 @@ if (!empty($_GET['add-song'])) {
 if (!empty($_GET['pl-id'])) {
     $songs = CancionRepository::getAllSongs();
     include('View/cancionView.phtml');
+    die;
+}
+
+if (!empty($_GET['adm-song'])) {
+    $LIMIT = 6;
+    $offset = ($_GET['index'] - 1) * $LIMIT;
+    if (!empty($_POST['search-song'])) {
+        $filter = $_POST['search-song'];
+        $songsLimited = CancionRepository::getLimitedSongsFiltered($filter, $offset, $LIMIT);
+    } else {
+        $filter = '';
+        $songsLimited = CancionRepository::getLimitedSongs($offset, $LIMIT);
+    }
+    $nLinks = ceil(CancionRepository::getNumberLinks($filter) / 6);
+    include('View/adminCancionView.phtml');
+    die;
+}
+
+if (!empty($_GET['edit-s'])) {
+    $song = CancionRepository::getSongById($_GET['edit-s']);
+    include('View/editCancionView.phtml');
     die;
 }
 
--- Model/CancionRepository.php
+++ Model/CancionRepository.php
@@ -48,6 +48,30 @@ class CancionRepository
         return $songs;
     }
 
+    public static function getLimitedSongs($index, $limit)
+    {
+        $bd = Conectar::conexion();
+        $q = "SELECT * FROM canciones LIMIT " . $index . ", " . $limit;
+        $result = $bd->query($q);
+        $songs = [];
+        while ($datos = $result->fetch_assoc()) {
+            $songs[] = new CancionClass($datos);
+        }
+        return $songs;
+    }
+
+    public static function getLimitedSongsFiltered($filter, $index, $limit)
+    {
+        $bd = Conectar::conexion();
+        $q = "SELECT * FROM canciones WHERE title LIKE '%" . $filter . "%' OR author LIKE '%" . $filter . "%' LIMIT " . $index . ", " . $limit;
+        $result = $bd->query($q);
+        $songs = [];
+        while ($datos = $result->fetch_assoc()) {
+            $songs[] = new CancionClass($datos);
+        }
+        return $songs;
+    }
+
     public static function addSongToPlaylist($playlist_id, $song_id)
     {
         $bd = Conectar::conexion();
@@ -59,6 +83,31 @@ class CancionRepository
     {
         $bd = Conectar::conexion();
         $q = "DELETE FROM song_in_playlist WHERE id_playlist=" . $playlist_id . " AND id_song=" . $song_id;
+        $bd->query($q);
+    }
+
+    public static function getNumberLinks($filter)
+    {
+        $bd = Conectar::conexion();
+        $q = "SELECT COUNT(*) FROM canciones WHERE title LIKE '%" . $filter . "%' OR author LIKE '%" . $filter . "%'";
+        $result = $bd->query($q);
+        $datos = $result->fetch_array();
+        return $datos[0];
+    }
+
+    public static function getSongById($song_id)
+    {
+        $bd = Conectar::conexion();
+        $q = "SELECT * FROM canciones WHERE id=" . $song_id;
+        $result = $bd->query($q);
+        $datos = $result->fetch_assoc();
+        return new CancionClass($datos);
+    }
+
+    public static function updateSong($id, $title, $author, $mp3)
+    {
+        $bd = Conectar::conexion();
+        $q = "UPDATE canciones SET title ='" . $title . "', author='" . $author . "', mp3='" . $mp3 . "' WHERE id=" . $id;
         $bd->query($q);
     }
 }
